3ba3893fa893a8d0c2a0173dd5364a932f40557b
Daniel Brodsky <d6brodsky@gmail.com>
2019-01-27 04:58:08 -0800

    Feat: added function nodes and relations to services

--- index.js
+++ index.js
@@ -18,11 +18,23 @@ const { lstatSync, readdirSync  } = require('fs')
 const { join  } = require('path')
 
 const isDirectory = source => lstatSync(source).isDirectory()
+
 const getDirectories = source =>
-      readdirSync(source).map(name => join(source, name)).filter(isDirectory)
+    readdirSync(source).map(name => join(source, name)).filter(isDirectory)
+
+const getFilesInDir = source =>
+    readdirSync(source).map(name => join(source, name))
+
 const getServiceNames = source =>
     getDirectories(source).map(name => name.split('/')[1])
 
+const getFunctionPaths = (source, services) => {
+    var functionPaths = [];
+    services.forEach((s) => {
+        functionPaths = functionPaths.concat(getFilesInDir(`${source}/${s}/functions/`));
+    })
+    return functionPaths;
+}
 
 function sendQuery(query, params) {
 
@@ -38,7 +50,6 @@ function sendQuery(query, params) {
         resultPromise.then(result => {
             session.close();
 
-            console.log("query successful");
             resolve(result);
 
         }).catch(error => {
@@ -51,29 +62,83 @@ function sendQuery(query, params) {
 }
 
 function createNode(nodeName, nodeType, nodeData) {
-
+    return new Promise(resolve => {
         sendQuery(
-            `CREATE (${nodeName}:${nodeType} {name: $name}) RETURN ${nodeName}`,
+            `CREATE (:${nodeType} {name: $name}) RETURN "${nodeName}"`,
             {name: nodeName}
-        );
+        ).then(() => resolve());
+    });
 }
 
-async function nodeExists(nodeName){
+function createRelation(fromNode, toNode, relation) {
+
+    return new Promise(resolve => {
+        sendQuery(
+            `MATCH (a),(b)
+        WHERE a.name = "${fromNode}" AND NOT (a)-->() AND b.name = "${toNode}"
+        CREATE (a)-[:${relation}]->(b)`
+        ).then(() => resolve());
+    });
+}
+
+
+function createFunction(fn, type, data) {
+    return new Promise(resolve => {
+        createNode(fn, type, "").then(() => {
+            createRelation(fn, data.service, "IN").then(() => {
+                resolve();
+            })
+        })
+    })
+}
+
+async function serviceExists(nodeName) {
     let result = await sendQuery(
         `MATCH (n) WHERE n.name = "${nodeName}" RETURN count(n) > 0 as n`
     );
-    console.log(Boolean(result.records[0]._fields[0]));
-
-    return result.records[0]._fields[0] ? false : true;
+    return new Promise((resolve) => {
+        resolve(result.records[0]._fields[0]);
+    })
 }
 
-function parseAllServices() {
+async function functionExists(fn, srv) {
 
-    const serviceNames = getServiceNames('./' + STDLIB_USER);
-    serviceNames.forEach( (service) => {
-        if (nodeExists(service))
-            createNode(service, "service", "");
+    let result = await sendQuery(
+        `MATCH (n:function)-[:IN]->(s:service) WHERE n.name = "${fn}"
+         AND s.name = "${srv}"
+        RETURN count(n) > 0 as n`
+    );
+    return new Promise((resolve) => {
+        resolve(result.records[0]._fields[0]);
     })
+}
+
+async function parseAllServices() {
+
+    // create service nodes if they don't already exist
+    const serviceNames = getServiceNames('./' + STDLIB_USER);
+    for (const service of serviceNames){
+        let exists = await serviceExists(service);
+        if (!exists){
+            console.log("adding service");
+            createNode(service, "service", "");
+        }
+    }
+
+    // create function nodes found in services
+    const functionNames = getFunctionPaths('./' + STDLIB_USER, serviceNames);
+    console.log(functionNames);
+    for (const fnPath of functionNames){
+        console.log(fnPath);
+        let fn = fnPath.split('/')[3];
+        let srv = fnPath.split('/')[1];
+        let exists = await functionExists(fn, srv);
+        if (!exists){
+            console.log(fn + "doesn't exist");
+            console.log("adding function");
+            await createFunction(fn, "function", {service: srv});
+        }
+    }
 }
 
 function main() {
